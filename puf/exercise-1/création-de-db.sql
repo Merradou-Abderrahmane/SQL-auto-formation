c3d59c3afa62fd9131bff132d40a0a064c173500
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-06 10:00:14 +0000

    create db and usine table

--- puf/exercise-1/création-de-db.sql
+++ puf/exercise-1/création-de-db.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE PUF;
+
+-- Create tables
+
+USE puf;
+CREATE TABLE usine( Num int(11) PRIMARY KEY AUTO_INCREMENT ,
+                    Nom varchar(30),
+                    Ville varchar(30));

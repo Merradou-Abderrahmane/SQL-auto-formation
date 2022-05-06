c9b4a9309f17d544e45fd3ab604e78fef9a4bf6a
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-06 10:17:23 +0000

    save

--- puf/exercise-1/création-de-db.sql
+++ puf/exercise-1/création-de-db.sql
@@ -1,3 +1,4 @@
+-- create db
 CREATE DATABASE PUF;
 
 -- Create tables
@@ -6,3 +7,24 @@ USE puf;
 CREATE TABLE usine( Num int(11) PRIMARY KEY AUTO_INCREMENT ,
                     Nom varchar(30),
                     Ville varchar(30));
+
+use puf;
+CREATE TABLE produit( Num int(11) PRIMARY KEY AUTO_INCREMENT,
+                      Nom varchar(70),
+                      Couleur varchar(70),
+                      poids varchar(255));
+
+use puf;
+CREATE TABLE fournisseur( Num int(11) PRIMARY KEY AUTO_INCREMENT,
+                          Nom varchar(70),
+                          Statut varchar(255),
+                          Ville varchar(70));
+
+use puf;
+CREATE TABLE Puf( NumP int(11),
+                  NumU int(11),
+                  NumF int(11),
+                  quantité decimal(65),
+                  FOREIGN KEY (NumP) REFERENCES produit(num),
+                  FOREIGN KEY (NumU) REFERENCES usine(num),
+                  FOREIGN KEY (NumF) REFERENCES fournisseur(num));

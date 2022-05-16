165f26c2ff7b58adf6802b18e2018fbd629c3432
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-16 09:47:03 +0100

    save

--- Exercise1/requete7.sql
+++ Exercise1/requete7.sql
@@ -1,2 +1,2 @@
 -- Donnez les différents proffessions et leur salaire moyen.
-SELECT prof , AVG(sal) AS "average" FROM employés GROUP BY prof;
+SELECT prof , AVG(sal) AS "average salary" FROM employés GROUP BY prof;
--- Exercise1/requete8.sql
+++ Exercise1/requete8.sql
@@ -1,2 +1,3 @@
 -- Donnez le plus bas salaire moyen par proffesion.
-SELECT prof, MIN(sal) AS "minimum" FROM employés GROUP BY prof;
+SELECT min(avg) AS "MinAverageSalary"  
+FROM ( SELECT avg(sal) AS "avg" FROM employés GROUP BY prof ) AS AverageSalary;
